342d0d68af1595511bedacc134d28e890ecfa225
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-11-10 23:48:16 +0500

    refactor timeout

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const T = new Twit({
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 20000
+  timeout_ms: 60 * 1000
 });
 
 const appleStream = T.stream('statuses/filter', {track: ['#apple', '#Apple', '#MacBook'], language: 'en'});
