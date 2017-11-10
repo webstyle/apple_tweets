4d37bff7905a57472e77ca74742e961e29894798
Farrukh Mamatkhalilov <webstyle@protonmail.com>
2017-11-11 00:01:27 +0500

    added uptime

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 
 const server = http.createServer((req, res) => {
-  res.end('OK');
+  res.end(`OK. uptime: ${new Date()}`);
 });
 
 server.listen(process.env.PORT || 5000);
